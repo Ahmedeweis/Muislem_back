f92ea8bddd6d61cc0e701147418f999140884a2e
Ahmedeweis <93427804+Ahmedeweis@users.noreply.github.com>
2026-02-21 04:51:52 +0200

    new

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const startServer = async () => {
     const swaggerSpec = swaggerJsdoc(swaggerOptions);
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
   } catch (err) {
     console.error('DB connection error:', err);
     process.exit(1); // هيقفل السيرفر لو فيه مشكلة
